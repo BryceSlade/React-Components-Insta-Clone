56804ca813730957e3a2f4bc10221b517ff15d17
Bryce Slade <bryceslade@gmail.com>
2021-10-05 14:46:47 -0600

    fine tuning

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,6 +1,27 @@
+// import React from 'react';
+// import Comment from './Comment';
+// import './Comments.css';
+
+// const Comments = props => {
+//   // 🔥 Make sure the parent of Comments is passing the right props!
+//   const { comments } = props;
+
+//   return (
+//     <div>
+//       {comments.map = (comments => {
+//         return <Comment comment={comments}/>
+//       })}
+//       {/* map through the comments prop and render a Comment for every piece of data */}
+//     </div>
+//   );
+// };
+
+// export default Comments;
+
 import React from 'react';
 import Comment from './Comment';
 import './Comments.css';
+
 
 const Comments = props => {
   // 🔥 Make sure the parent of Comments is passing the right props!
@@ -8,7 +29,9 @@ const Comments = props => {
 
   return (
     <div>
-      {/* map through the comments prop and render a Comment for every piece of data */}
+      {comments.map((coms) => {
+        return <Comment comment ={coms} />;
+      })};
     </div>
   );
 };
